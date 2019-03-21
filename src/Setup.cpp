ad82d691ec290f2aa694a72b86417dc01d52cbee
demisquare <demigliomaria@libero.it>
2019-03-21 16:32:07 +0100

    Fixed Mouse Position.

--- src/Setup.cpp
+++ src/Setup.cpp
@@ -4,14 +4,14 @@ void Setup::init_display()
 {
   al_set_new_display_flags(ALLEGRO_FULLSCREEN_WINDOW);
 
-  display = al_create_display(windowWidth, windowHeight);
+  display = al_create_display(WIDTH, HEIGHT);
   if(!display)
   {
-   cerr << "failed to initialize allegro!\n";
+   cerr << "Fatal error, unable to create a display\n";
    exit(-1);
   }
   
-  buffer = al_create_bitmap(windowWidth, windowHeight);
+  buffer = al_create_bitmap(WIDTH, HEIGHT);
   if(!buffer)
   {
     cout << "Fatal error, unable to create a buffer\n";
@@ -19,15 +19,19 @@ void Setup::init_display()
   }
   
   //operazione di resize...
-  windowWidth = al_get_display_width(display);
-  windowHeight = al_get_display_height(display);
-  sx = windowWidth/(float)WIDTH;
-  sy = windowHeight/(float)HEIGHT;
-  scale = std::min(sx, sy);
-  scaleW = WIDTH * scale;
-  scaleH = HEIGHT * scale;
-  scaleX = (windowWidth - scaleW)/2;
-  scaleY = (windowHeight - scaleH)/2;
+   windowHeight = al_get_display_height(display);
+   windowWidth = al_get_display_width(display);
+
+   sx = windowWidth / float(WIDTH);
+   sy = windowHeight / float(HEIGHT);
+
+   scale = std::min(sx, sy);
+
+   scaleW = WIDTH * scale;
+   scaleH = HEIGHT * scale;
+
+   scaleX = (windowWidth - scaleW) / 2;
+   scaleY = (windowHeight - scaleH) / 2;
 
   al_set_target_bitmap(buffer);
   al_clear_to_color(BLACK);
@@ -93,36 +97,33 @@ void Setup::drawMenu()
   al_draw_scaled_bitmap(buffer, 0, 0, WIDTH, HEIGHT, scaleX, scaleY, scaleW, scaleH, 0);
   al_flip_display();
 
-  al_set_target_bitmap(buffer);
-
   al_register_event_source(event_queue, al_get_mouse_event_source());
   al_register_event_source(event_queue, al_get_keyboard_event_source());
 
-  bool done = false;
+  al_set_target_bitmap(buffer);
 
-  while(!done)
+
+  while(true)
   {
     menu = al_load_bitmap("../images/background.png");
 
     if(!menu)
       exit(-1);
 
-    al_clear_to_color(al_map_rgb(0, 0, 0));
+    al_clear_to_color(BLACK);
     al_draw_bitmap(menu, 0, 0, 0);
-
-    //al_draw_filled_rectangle(633, 423, 736, 460, PINK);
 
     ALLEGRO_EVENT ev;
     al_wait_for_event(event_queue, &ev);
 
-    unsigned mouseX = ev.mouse.x-365;
-    unsigned mouseY = ev.mouse.y-140;
+    int mouseX = ev.mouse.x - scaleX;
+    int mouseY = ev.mouse.y - scaleY;
 
     switch (ev.type)
     {
       case ALLEGRO_EVENT_MOUSE_AXES: //movimento mouse
-      //al_draw_filled_circle(ev.mouse.x, ev.mouse.y, 1, WHITE);
-      //al_draw_filled_circle(mouseX, mouseY, 7, RED);
+      //al_draw_filled_circle(ev.mouse.x, ev.mouse.y, 7, WHITE); //coordinate effettive
+      //al_draw_filled_circle(mouseX, mouseY, 3, RED); //coordinate traslate
 
       //sono su "Play!"
       if(mouseX >= 275 &&
@@ -132,26 +133,26 @@ void Setup::drawMenu()
       
 	      al_draw_filled_circle(252, 300, 7, PINK);
 
+      //sono su "Options"
+      else if (mouseX >= 260 &&
+               mouseX <= 379 &&
+               mouseY >= 330 &&
+               mouseY <= 363)
       
-        else if (mouseX >= 260 &&
-                 mouseX <= 379 &&
-                 mouseY >= 330 &&
-                 mouseY <= 363)
-        
-          al_draw_filled_circle(252, 350, 7, GREEN);
-        
-        
-        //sono su "Quit"
-        else if (mouseX >= 283 &&
-                 mouseX <= 356 &&
-                 mouseY >= 421 &&
-                 mouseX <= 460)
-        
-					al_draw_filled_circle(267, 444, 7, RED);
-            
-            
-        else
-          al_set_target_bitmap(buffer);
+        al_draw_filled_circle(252, 350, 7, GREEN);
+      
+      
+      //sono su "Quit"
+      else if (mouseX >= 283 &&
+               mouseX <= 356 &&
+               mouseY >= 421 &&
+               mouseX <= 460)
+      
+				al_draw_filled_circle(267, 444, 7, RED);
+          
+          
+      else
+        al_set_target_bitmap(buffer);
             
       break;
       
@@ -161,7 +162,7 @@ void Setup::drawMenu()
 
       case ALLEGRO_EVENT_KEY_DOWN:
       if (ev.keyboard.keycode == ALLEGRO_KEY_ESCAPE)
-           done = true;
+           exit(1);
         break;
 
       default:
