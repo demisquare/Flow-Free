6964db2f03c6fa08e90001559550218c35ffdbf5
demisquare <demigliomaria@libero.it>
2019-03-22 10:23:06 +0100

    Main.

--- src/FlowFree.cpp
+++ src/FlowFree.cpp
@@ -1,14 +1,9 @@
 #include "../head/Setup.h"
-#include "../head/Menu.h"
-
 
 int main()
 {
   Setup mySetup;
   mySetup.drawMenu();
-
-  //Level myLevel;
-  //myLevel.run("../levels/level1.txt");
   
   return 0;
 }
